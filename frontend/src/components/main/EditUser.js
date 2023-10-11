12b85dd370b288cce69daa52a9f929163049c790
nikolay <89162450327@ya.ru>
2023-10-11 22:44:44 +0300

    добавил выпадающий список с ролью

--- frontend/src/components/main/EditUser.js
+++ frontend/src/components/main/EditUser.js
@@ -7,17 +7,29 @@ class EditUser extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: "",
-            role: ""
+            name: this.props.user.name,
+            role: this.props.user.role
         }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({role: event.target.value})
     }
 
     render() {
         return (
             <form ref={(el) => this.myForm = el}>
-                <input placeholder="Никнейм" onChange={(e) => this.setState({name: e.target.value})} />
-                <input placeholder="роль" onChange={(e) => this.setState({role: e.target.value})} />
+                <input value={this.state.name} placeholder="Никнейм" onChange={(e) => this.setState({name: e.target.value})} />
                 
+                <select value={this.state.role} onChange={this.handleChange}>
+                        <option value="ROLE_ADMIN">Аминистратор</option>
+                        <option value="ROLE_TRAINER">Тренер</option>
+                        <option value="ROLE_USER">Пользователь</option>
+                        <option value="ROLE_PREMIUM">Премиум пользователь</option>
+                </select>
+
                 <Button doIt={() => {
                     this.myForm.reset()
                     this.userAdd = {
